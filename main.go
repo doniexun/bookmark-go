8a9c30556352683de1cc7b3f5bee308f826e14db
GallenHu <huguiliang@zbj.com>
2018-04-04 14:07:27 +0800

    init restful project

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/GallenHu/bookmarkgo/common"
-	"github.com/GallenHu/bookmarkgo/routes"
 )
 
 func main() {
-	configPath := flag.String("c", "./conf.json", "config file path")
-	flag.Parse() // 执行命令行解析
-
-	config := common.LoadConfig(*configPath)
-	fmt.Println(config.Name)
-
-	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
-
-	router.Static("/static", "./static")
-	routes.Register(router)
-
-	router.Run(":8080")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"msg": "hello world",
+		})
+	})
+	r.Run(":3000") // :8080
 }
--- model/user.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "github.com/jinzhu/gorm"
-
-type User struct {
-	gorm.Model
-	Mail		string	`gorm:"unique_index;not null;type:varchar(50)"`
-	Password	string	`gorm:"not null;type:varchar(50)"`
-}
